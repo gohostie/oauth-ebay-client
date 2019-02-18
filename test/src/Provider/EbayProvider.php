ceedec249a9d06236b02e98a884cfe796348eaf0
gohostie <47715633+gohostie@users.noreply.github.com>
2019-02-18 14:30:40 +0000

    Add files via upload

---
+++ test/src/Provider/EbayProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GOhostie\OAuth2\Client\Provider;
+/**
+
+use InvalidArgumentException;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+ */
+class EbayProvider extends AbstractProvider
+{
+
+    /**
+     * @param array $options
+     * @param array $collaborators
+     */
+    public function __construct(array $options = [], array $collaborators = [])
+    {
+    }
+}
