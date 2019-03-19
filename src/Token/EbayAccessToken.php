542b6114fa3cd3a23c50f1146684eb8266bc788a
gohostie <47715633+gohostie@users.noreply.github.com>
2019-03-19 17:20:06 +0000

    Add files via upload

---
+++ src/Token/EbayAccessToken.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gohostie\OAuth2\Client\Token;
+use League\OAuth2\Client\Token\AccessToken;
+class EbayAccessToken extends AccessToken
+{
+    /**
+     * @param string $resourceOwnerId
+     */
+    public function setResourceOwnerId($resourceOwnerId)
+    {
+        $this->resourceOwnerId = $resourceOwnerId;
+    }
+}
